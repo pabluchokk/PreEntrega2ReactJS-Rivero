d3fc5a6cafd7ecfaa2a03f9e15460870b8f6bb63
pabluchokk <pabloagusrivero17@gmail.com>
2023-10-16 16:35:21 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Nav from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+
+const Nav = () => {
+    return (
+    <div>
+        <header className="navi">
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Catálogo</li>
+                    <li>Contacto</li>
+                    <li>Perfil</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    );
+}
+
+export default Nav;
